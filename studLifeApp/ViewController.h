a1dc849ef57b7e8f996324d7855a42c7b5ba2b42
Labuser <Labuser@wtk316-28.cec.wustl.edu>
2015-11-17 18:29:19 -0600

    Replace with Xcode 6.4 app

--- studLifeApp/ViewController.h
+++ studLifeApp/ViewController.h
@@ -2,13 +2,15 @@
 //  ViewController.h
 //  studLifeApp
 //
-//  Created by Labuser on 11/17/15.
-//  Copyright (c) 2015 WUSTL. All rights reserved.
+//  Created by Sean McManus on 11/15/15.
+//  Copyright © 2015 Sean McManus. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSString *content;
+}
 
 
 @end
